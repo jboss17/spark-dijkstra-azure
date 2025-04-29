eee727f2999256cc5003cd93a93698e7e532fbee
jonathanboss17 <jtboss47@gmail.com>
2025-04-29 00:08:26 -0400

    more script changes

--- scripts/install_spark.sh
+++ scripts/install_spark.sh
@@ -11,13 +11,13 @@ wget https://archive.apache.org/dist/spark/spark-3.4.1/spark-3.4.1-bin-hadoop3.t
 
 echo "🛠 Extracting Spark..."
 tar xvf spark-3.4.1-bin-hadoop3.tgz
-mv spark-3.4.1-bin-hadoop3 spark
+mv spark-3.4.1-bin-hadoop3 ~/spark
 
 # === Part 3: Set Environment Variables ===
 echo "🔧 Setting SPARK_HOME and PATH..."
 if ! grep -q "export SPARK_HOME=~/spark" ~/.bashrc; then
-    echo 'export SPARK_HOME=~/spark' >> ~/.bashrc
-    echo 'export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin' >> ~/.bashrc
+    echo "export SPARK_HOME=~/spark" >> ~/.bashrc
+    echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" >> ~/.bashrc
 fi
 
 # Load new environment variables
